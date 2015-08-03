43946f00cbac5d59bd9025ad568cc65f7bc7f0f2
root <root@linux-build.(none)>
2015-08-03 12:26:17 +0200

    "New build based on revison 20983"

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -1,7 +1,7 @@
 #ifndef YOCTO_API_BUILD_NO
-#define YOCTO_API_BUILD_NO          "20975"
+#define YOCTO_API_BUILD_NO          "20983"
 #endif
-#define YBUILD_20975
+#define YBUILD_20983
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
