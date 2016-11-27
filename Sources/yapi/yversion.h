935c5f2c3a0443a810cc4f4c2670255df5e28456
Ludovic Rousseau <ludovic.rousseau@free.fr>
2016-11-27 15:34:02 +0100

    Fix compiler warning: invalid suffix on literal
    ../Sources/yapi/yversion.h:8:39: warning: invalid suffix on literal; C++11 requires a space between literal and string macro [-Wliteral-suffix]
     #define YOCTO_API_BUILD_DATE          ""__DATE__" "__TIME__""
                                           ^

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -5,7 +5,7 @@
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
-#define YOCTO_API_BUILD_DATE          ""__DATE__" "__TIME__""
+#define YOCTO_API_BUILD_DATE          "" __DATE__ " " __TIME__ ""
 #else
 #define YOCTO_API_BUILD_DATE          "PATCH_WITH_DATE"
 #endif
