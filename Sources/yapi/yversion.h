4e920da2fcfd6bda56e55ea4570bfb905bcc1a84
Yoctopuce <dev@yoctopuce.com>
2019-06-04 17:50:02 +0200

    "New build based on revison 35635"

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -1,7 +1,7 @@
 #ifndef YOCTO_API_BUILD_NO
-#define YOCTO_API_BUILD_NO          "35622"
+#define YOCTO_API_BUILD_NO          "35635"
 #endif
-#define YBUILD_35622
+#define YBUILD_35635
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
