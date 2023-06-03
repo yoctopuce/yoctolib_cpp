16f8b861099ed51de68ea052d8de859b8083e869
Yoctopuce <dev@yoctopuce.com>
2023-06-03 22:24:52 +0200

    "New build based on revison 54852"

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -1,7 +1,7 @@
 #ifndef YOCTO_API_BUILD_NO
-#define YOCTO_API_BUILD_NO          "54821"
+#define YOCTO_API_BUILD_NO          "54852"
 #endif
-#define YBUILD_54821
+#define YBUILD_54852
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
