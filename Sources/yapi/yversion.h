c0356d114ad0b17859dd0fbb6ce6be9bd185394f
Yoctopuce <dev@yoctopuce.com>
2024-02-13 12:24:20 +0100

    "New build based on revision 59348"

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -1,7 +1,7 @@
 #ifndef YOCTO_API_BUILD_NO
-#define YOCTO_API_BUILD_NO          "59303"
+#define YOCTO_API_BUILD_NO          "59348"
 #endif
-#define YBUILD_59303
+#define YBUILD_59348
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
