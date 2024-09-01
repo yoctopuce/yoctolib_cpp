edca2fd98e26dfc2cc44059898fb66aa0d3c3d89
Yoctopuce <dev@yoctopuce.com>
2024-09-01 18:30:06 +0200

    "New build based on revision 62367"

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -1,7 +1,7 @@
 #ifndef YOCTO_API_BUILD_NO
-#define YOCTO_API_BUILD_NO          "62334"
+#define YOCTO_API_BUILD_NO          "62367"
 #endif
-#define YBUILD_62334
+#define YBUILD_62367
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
