ce354838976eed3f2ccb602db790ee058803ea03
Yoctopuce <dev@yoctopuce.com>
2024-09-25 08:41:40 +0200

    "New build based on revision 62733"

--- Sources/yapi/yversion.h
+++ Sources/yapi/yversion.h
@@ -1,7 +1,7 @@
 #ifndef YOCTO_API_BUILD_NO
-#define YOCTO_API_BUILD_NO          "62710"
+#define YOCTO_API_BUILD_NO          "62733"
 #endif
-#define YBUILD_62710
+#define YBUILD_62733
 
 #ifndef YOCTO_API_BUILD_DATE
 #ifndef RC_INVOKED
